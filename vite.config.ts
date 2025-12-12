5d0503d31e400fcb7c6e9a3088387ff1c76361c9
Moh. Naufal Sadid <naufalsadid@gmail.com>
2025-12-13 01:36:38 +0700

    edit geminiservice

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig(({ mode }) => {
       base: '/colorchanger/', 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'import.meta.env.VITE_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
